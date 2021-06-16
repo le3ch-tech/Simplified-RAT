43c01cd246b3a2ec6b2608ede12722aee0051c7f
le3ch-tech <sam.moes15@gmail.com>
2021-06-16 16:33:30 +0200

    Files & GUI

--- main.py
+++ main.py
@@ -5,6 +5,14 @@ from time import sleep
 
 gitrepo = 'https://github.com/le3ch-tech/Simplified-RAT.git'
 
+def pyfi_install_():
+    if os.name == 'nt':
+        pyfi_install_ = 'pip install pyfiglet'
+        return pyfi_install_
+    else:
+        pyfi_install_ = 'python3 -m pip install pyfiglet'
+        return pyfi_install_
+
 try:
     import pyfiglet
     from gui.front import *
@@ -12,10 +20,23 @@ try:
 except ModuleNotFoundError:
     print('Modules not installed!')
     sleep(1)
-    if ModuleNotFoundError() == gui.front:
-        print('GUI isnt installed. Please reinstall')
+    if ModuleNotFoundError() == gui.front | ModuleNotFoundError() == server.access_point:
+        print('GUI or SERVER isnt installed. Please reinstall')
         print('git clone: {}'.format(gitrepo))
-        print('closing in 3 seconds')
+        print('Closing in 3 seconds')
         sleep(3)
         exit()
-    
+    else:
+        print('Module: Pyfiglet not installed.')
+        print('Would you like to install it?')
+        pyfi_ = input('Y / N')
+        if pyfi_ == 'y' | pyfi_ == 'Y':
+            print('Installing...')
+            sleep(1)
+            system.os(pyfi_install_())
+        else:
+            print('Closing in 3 seconds')
+            sleep(3)
+            exit()
+            
+            
